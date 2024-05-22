b56ee8cdeafa8b6b839319f7e3fad370b88acf4f
baglanz <baglan.zhaksylyk98@gmail.com>
2024-05-22 16:12:04 +0500

    A Needle in the Haystack

--- 8kyu/A_Needle_in_the_Haystack.php
+++ 8kyu/A_Needle_in_the_Haystack.php
@@ -1,50 +1,24 @@
-# Codewars PHP
+8Kyu - A Needle in the Haystack
 
----
-
-## 8Kyu - A Needle in the Haystack
-
-**DESCRIPTION:**
-
+DESCRIPTION:
 Can you find the needle in the haystack?
-
-Write a function `findNeedle()` that takes an `array` full of junk but containing one `"needle"`
-
+Write a function findNeedle() that takes an array full of junk but containing one "needle"
 After your function finds the needle it should return a message (as a string) that says:
-
-`"found the needle at position "` plus the `index` it found the needle, so:
-
+"found the needle at position " plus the index it found the needle, so:
 Example(Input --> Output)
 
-```PHP
 ["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"] --> "found the needle at position 5"
-```
+Note: In COBOL, it should return "found the needle at position 6"
 
----
-
-### Given Code
-
-
-```PHP
-function findNeedle($haystack) {
-// your code here
-}
-```
-
----
-
-### Solution
-
-
-```PHP
-function findNeedle($haystack) {
-    foreach($haystack as $index => $value) {
-        if ($value === 'needle') return "found the needle at position ".$index;
+<?php
+function findNeedle($haystack)
+{
+    foreach ($haystack as $index => $value) {
+        if ($value === 'needle') return "found the needle at position " . $index;
     }
+    return 'needle not found';
 }
-```
 
----
-
+?>
 
 [See on CodeWars.com](https://www.codewars.com/kata/56676e8fabd2d1ff3000000c)
