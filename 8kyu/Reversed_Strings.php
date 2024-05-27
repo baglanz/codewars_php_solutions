55e7f7dc85c7147d0ccd6d21f2cb02601612a2d5
baglanz <baglan.zhaksylyk98@gmail.com>
2024-05-27 16:28:40 +0500

    Reversed Strings

---
+++ 8kyu/Reversed_Strings.php
@@ -0,0 +1,15 @@
+8Kyu - Reversed Strings
+
+Description:
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+
+<?php
+function solution($str) {
+    return strrev($str);
+}
+?>
+
+See on CodeWars.com: https://www.codewars.com/kata/5168bb5dfe9a00b126000018
