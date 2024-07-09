d525b47315537ba34472e572505d0c64166e6b60
baglanz <baglan.zhaksylyk98@gmail.com>
2024-07-09 22:36:45 +0500

    Human Readable Time

---
+++ 5kyu/Human_Readable_Time.php
@@ -0,0 +1,22 @@
+5kyu - Human Readable Time
+
+Description:
+
+Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
+
+HH = hours, padded to 2 digits, range: 00 - 99
+MM = minutes, padded to 2 digits, range: 00 - 59
+SS = seconds, padded to 2 digits, range: 00 - 59
+The maximum time never exceeds 359999 (99:59:59)
+
+<?php
+function human_readable($seconds) {
+    $hours = floor($seconds / 3600);
+    $minutes = floor(($seconds % 3600) / 60);
+    $remainingSeconds = $seconds % 60;
+
+    return sprintf('%02d:%02d:%02d', $hours, $minutes, $remainingSeconds);
+}
+?>
+
+See on CodeWars.com: https://www.codewars.com/kata/52685f7382004e774f0001f7
